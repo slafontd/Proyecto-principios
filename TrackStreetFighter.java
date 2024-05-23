0d916229c049bfaab26c0b1bbe8e765cba73727e
slafontd <160205483+slafontd@users.noreply.github.com>
2024-05-23 10:18:24 -0500

    Update and rename Programa.java to TrackStreetFighter.java

---
+++ TrackStreetFighter.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.trackstreetfighter;
+
+/**
+ *
+ * @author Windows
+ */
+public class TrackStreetFighter {
+
+    public static void main(String[] args) {
+        front form1 = new front();
+        form1.setVisible(true);
+    }
+}
+
